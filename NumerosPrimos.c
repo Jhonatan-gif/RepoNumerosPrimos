48465de68ff122a9d02b18a14772185591020890
Jhonatan Tipan <laboratorio@udla-ec.int>
2023-10-04 16:46:01 -0500

    implementacion de ejercicios

--- NumerosPrimos.c
+++ NumerosPrimos.c
@@ -1,12 +1,23 @@
 #include <stdio.h>
 
-int main (){
-    int numero;
-
-    printf("Ingrese un numero: ");
-    scanf ("%d", &numero);
-
+int esPrimo(int numero) {
+    if (numero <= 1) {
+        return 0; 
+    }
+    for (int i = 2; i * i <= numero; i++) {
+        if (numero % i == 0) {
+            return 0; 
+        }
+    }
     
-
-    return 0 ;
+    return 1;
+}
+int main() {
+    int numero;
+    printf("Ingrese un número: ");
+    scanf("%d", &numero);
+    
+    
+    
+    return 0;
 }
